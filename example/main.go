2b4a80f9de03c8bd787a608d694938cfe16b7f86
Puja Kusuma <pujakusumae@hotmail.com>
2021-07-14 12:34:22 +0700

    remove spew dependency

--- example/main.go
+++ example/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/davecgh/go-spew/spew"
 	sdk "github.com/pudjamansyurin/gen_vcu_sdk"
+	"github.com/pudjamansyurin/gen_vcu_sdk/util"
 )
 
 func main() {
@@ -13,5 +13,5 @@ func main() {
 }
 
 func reportListener(report interface{}, bytes []byte) {
-	spew.Dump(report)
+	util.Debug(report)
 }
