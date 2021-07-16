dd87dc0fb3cebfe18f5b5ec096b561890ab6b4ef
Puja Kusuma <pujakusumae@hotmail.com>
2021-07-16 10:14:57 +0700

    Use shared enums in command list

--- command/validator.go
+++ command/validator.go
@@ -1,5 +1,14 @@
 package command
 
+func contains(b []byte, value ...uint8) bool {
+	for _, v := range value {
+		if toUint8(b) == v {
+			return true
+		}
+	}
+	return false
+}
+
 func max(b []byte, max uint8) bool {
 	return toUint8(b) <= max
 }
